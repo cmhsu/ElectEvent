9ca87fb1ed06e460642cdab175b9f8d2dd89131d
Andrew Weber <andrew.robert.weber@gmail.com>
2015-08-05 15:50:23 -0700

    sending sample data from app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/app'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res, next) {
   res.sendStatus(200);
@@ -11,28 +13,97 @@ app.post('/', function (req, res, next) {
   res.sendStatus(200);
 });
 
-app.get('/users', function (req, res, next) {
-  res.send("It works");
+app.get('/api/users', function (req, res, next) {
+  var sampleUsers = [
+    {
+      "id": 1,
+      "username": "SAMPLE_NAME",
+      "groups": ["group_id1", "groupid2"]
+    },
+    {
+      "id": 2,
+      "username": "SAMPLE_NAME_2",
+      "groups": ["group_id1", "groupid2"]
+    }
+  ];
+  res.json(sampleUsers);
 });
 
-app.post('/users', function (req, res, next) {
+app.post('/api/users', function (req, res, next) {
   res.sendStatus(200);
 });
 
-app.get('/posts', function (req, res, next) {
-
+app.get('/api/events', function (req, res, next) {
+  var sampleEvents = [
+    {
+      "id": 1,
+      "title": "An Event",
+      "group": "group id",
+      "description": "A great thing to do",
+      "comments": ["comment_id1", "comment_id2"],
+      "creator": "user_id",
+      "votes": 0
+    },
+    {
+      "id": 2,
+      "title": "Another Title",
+      "group": "group id",
+      "description": "A better thing to do",
+      "comments": ["comment_id1", "comment_id2"],
+      "creator": "user_id",
+      "votes": 0
+    }
+  ];
+  res.json(sampleEvents);
 });
 
-app.post('/posts', function (req, res, next) {
+app.post('/api/events', function (req, res, next) {
   res.sendStatus(200);
 });
 
-app.get('/groups', function (req, res, next) {
+app.get('/api/groups', function (req, res, next) {
+
+  var sampleGroups = [  
+    {
+      "id": 1,
+      "groupname": "Best Group",
+      "events": ["event_id1", "event_id2"],
+      "members": ["user_id1", "user_id2"]
+    },
+    {
+      "id": 1,
+      "groupname": "Bestester Group",
+      "events": ["event_id1", "event_id2"],
+      "members": ["user_id1", "user_id2"]
+    }
+
+  ];
+
+  res.json(sampleGroups);
 
 });
 
-app.post('/groups', function (req, res, next) {
+app.post('/api/groups', function (req, res, next) {
   res.sendStatus(200);
+});
+
+app.get('/api/comments', function (req, res, next) {
+  var sampleComments =    [
+    {
+      "id": 1,
+      "creator": "user_id",
+      "content": "Some text",
+      "event": "event_id"
+    },
+    {
+      "id": 2,
+      "creator": "user_id",
+      "content": "Moar text",
+      "event": "event_id"
+    }
+  ];
+
+  res.json(sampleComments);
 });
 
 app.listen(8000);
