63e0ddcda126414c2d429dd8e51fa0987588c771
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-05 19:50:58 -0700

    Feat: basic ui-router complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,13 @@
  * Main module of the application.
  */
 var app  = angular
-  .module('stationarySalmonBestSalmonApp', []);
+  .module('stationarySalmonBestSalmonApp', ['ui.router']);
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('state1');
+  $stateProvider.state('state1', {
+    url: '/state1',
+    templateUrl: '../../404.html'
+  });
+});
 
