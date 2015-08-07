6e65461899cef0675fef6565c0c8b399e1edda97
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-06 19:58:20 -0700

    (fix) Access creator username and comments

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -2,6 +2,8 @@ angular.module('app.event', [])
   .controller('EventController', ['$scope', 'EventService', '$routeParams', function($scope, EventService, $routeParams) {
     EventService.get($routeParams.id).then(function(data) {    
       $scope.eventName = data.title;
+      $scope.group = data.groups.groupname;
+      $scope.creator = data.creator.username;
       $scope.eventDescription = data.description;
       $scope.votes = data.vote;
       $scope.comments = data.comments;
