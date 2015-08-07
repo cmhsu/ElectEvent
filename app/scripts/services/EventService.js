fefa796de129cbc296c42a731d1c4588619fea22
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-07 15:23:02 -0700

    (fix)code clean-up post merge

--- app/scripts/services/EventService.js
+++ app/scripts/services/EventService.js
@@ -1,4 +1,4 @@
-app.event.service('EventService', ['$http', function($http) {
+app.service('EventService', ['$http', function($http) {
   this.get = function(eventId) {
     return $http.get('api/events/' + eventId).then(function(response) {
       return response.data;
