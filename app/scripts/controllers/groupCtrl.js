1605b444cea6a40c0f938863d9c45af56bbe7125
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-08-07 11:28:42 -0700

    add group feature

---
+++ app/scripts/controllers/groupCtrl.js
@@ -0,0 +1,10 @@
+app.controller('groupCtrl', ['$scope', '$routeParams', '$stateParams', 'getGroups', function ($scope, $routeParams, $stateParams, getGroups) {
+  var userId = $routeParams; // need to fill this out
+  $scope.meow = userId;
+
+  // getGroups.getGroup($routeParams.id).then(function (data) {
+  //   $scope.groupname = data.groupname;
+  //   $scope.events = data.events;
+  //   $scope.members = data.members;
+  // });
+}]);
