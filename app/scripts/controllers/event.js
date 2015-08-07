209a088f84784ae8d419a28e58f5c7775dfb94d4
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-07 15:11:47 -0700

    (feat)complete single event view w/ctrl + service

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -1,8 +1,8 @@
 app.controller('EventController', ['$scope', 'EventService', '$stateParams', function($scope, EventService, $stateParams) {
     EventService.get($stateParams.id).then(function(data) {    
       $scope.eventName = data.title;
-      $scope.group = data.group;
-      $scope.creator = data.creator;
+      $scope.group = data.group.groupname;
+      $scope.creator = data.creator.username;
       $scope.eventDescription = data.description;
       $scope.votes = data.vote;
       $scope.comments = data.comments;
