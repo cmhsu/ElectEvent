fb5d7504bb1b25fb4566795a6ae15b67312d1b04
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-08-07 15:09:29 -0700

    clean up view

--- app/scripts/controllers/groupCtrl.js
+++ app/scripts/controllers/groupCtrl.js
@@ -1,9 +1,6 @@
 app.controller('groupCtrl', ['$scope', '$stateParams', 'getGroups', function ($scope, $stateParams, getGroups) {
-  console.log($stateParams);
-
   getGroups.getGroup($stateParams.id).then(function (response) {
     var data = response.data;
-    console.log(data);
     $scope.groupname = data.groupname;
     $scope.comments = data.comments;
     $scope.events = data.events;
