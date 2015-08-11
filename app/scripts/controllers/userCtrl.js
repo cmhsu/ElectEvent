c0ba7f8ddd4a1dc98ee29204867dd103290b555e
Corey Hsu <corey81@gmail.com>
2015-08-11 16:36:07 -0700

    Implement some bootstrap styling. Add responsive navbar. Add page-connecting links to all views. Add some new seed data.

--- app/scripts/controllers/userCtrl.js
+++ app/scripts/controllers/userCtrl.js
@@ -2,6 +2,7 @@ app
   .controller('userCtrl', ['$scope', 'UserService', '$location', '$stateParams', function($scope, UserService, $location, $stateParams) {
     UserService.get($stateParams.id).then(function(response) {
       var data = response.data;
+      console.log(data);
       $scope.username = data.username;
       $scope.groups = data.groups;
     });
