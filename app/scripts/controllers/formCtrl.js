40b07d99ff017698a7cd0959f327d902a3387c91
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-12 15:15:13 -0700

    (feat)direct user to home after signup & return promise from service

--- app/scripts/controllers/formCtrl.js
+++ app/scripts/controllers/formCtrl.js
@@ -1,7 +1,9 @@
-app.controller('FormCtrl', ['$scope', 'FormService', function($scope, FormService) {
+app.controller('FormCtrl', ['$scope', 'FormService', '$location', function($scope, FormService, $location) {
   $scope.user = {};
   $scope.signup = function() {
     var user = $scope.user;
-    FormService.signup(user);
+    FormService.signup(user).then(function() {
+      $location.path('/');
+    });
   };
 }]);
