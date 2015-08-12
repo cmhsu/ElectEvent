56b2bf808b5f8e125480bd5d8253ab92e0e7a048
Andrew Weber <andrew.robert.weber@gmail.com>
2015-08-11 20:15:42 -0700

    adds username to comment

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -1,12 +1,23 @@
-app.controller('EventController', ['$scope', 'EventService', '$stateParams', function($scope, EventService, $stateParams) {
-    EventService.get($stateParams.id).then(function(response) {
-      var data = response.data;
-      $scope.data = data;
-      $scope.eventName = data.title;
-      $scope.group = data.group.groupname;
-      $scope.creator = data.creator.username;
-      $scope.eventDescription = data.description;
-      $scope.votes = data.vote;
-      $scope.comments = data.comments;
-    });
+app.controller('EventController', ['$scope', 'EventService', 'UserService', '$stateParams', 
+  function($scope, EventService, UserService, $stateParams) {
+    var populateEventData = function(){
+      EventService.get($stateParams.id).then(function(response) {
+        var data = response.data;
+        $scope.data = data;
+        $scope.eventName = data.title;
+        $scope.group = data.group.groupname;
+        $scope.creator = data.creator.username;
+        $scope.eventDescription = data.description;
+        $scope.votes = data.vote;
+        $scope.comments = data.comments;
+      })
+      .then(function(){
+        $scope.comments.forEach(function(comment){
+          UserService.get(comment.creator).then(function(response){
+            comment.creator = response.data.username;
+          });
+        });
+      });
+    }
+    populateEventData();
   }]);
