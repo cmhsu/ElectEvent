0b3efcc09740aaf6722478183a73aca1618942bd
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-08-16 12:23:13 -0700

    random changes

--- server/server.js
+++ server/server.js
@@ -29,7 +29,10 @@ app.use('/api/users', usersRoute);
 app.use('/api/groups', auth.isAuthorized, groupsRoute);
 app.use('/api/comments', auth.isAuthorized, commentsRoute);
 
-app.listen(8000, function(){
+app.listen(8000, function(err) {
+  if (err) {
+    console.log(err);
+  }
   console.log("Listening on Port 8000");
 });
 
