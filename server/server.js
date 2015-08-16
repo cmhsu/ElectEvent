2e1ff8b08a269d0a71c77452a9f6ae55cd85ca04
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-08-16 12:59:37 -0700

    remove error handler

--- server/server.js
+++ server/server.js
@@ -29,10 +29,7 @@ app.use('/api/users', usersRoute);
 app.use('/api/groups', auth.isAuthorized, groupsRoute);
 app.use('/api/comments', auth.isAuthorized, commentsRoute);
 
-app.listen(8000, function(err) {
-  if (err) {
-    console.log(err);
-  }
+app.listen(8000, function() {
   console.log("Listening on Port 8000");
 });
 
