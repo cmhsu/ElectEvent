332eae867778843e7a7171fed0bea5e0baba43d8
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-08-16 12:58:38 -0700

    add group create form

--- app/scripts/controllers/groupFormCtrl.js
+++ app/scripts/controllers/groupFormCtrl.js
@@ -1,7 +1,6 @@
 app
   .controller('groupFormCtrl', ['$scope', '$cookies', 'groupCreate', function ($scope, $cookies, groupCreate) {
     $scope.submitForm = function (groupName) {
-      // have to add user_id
       groupCreate.createGroup(groupName, $cookies.get('user_id'));
     }
   }]);
--- app/scripts/services/groupCreateService.js
+++ app/scripts/services/groupCreateService.js
@@ -1,7 +1,6 @@
 app.service('groupCreate', ['$http', function ($http) {
   this.createGroup = function (groupName, founderId) {
-    var groupData = {groupName: groupName, user_id: founderId};
-    // return $http.post('api/groups/', groupData);
-    console.log(groupName, founderId);
+    var groupData = {groupname: groupName, user_id: founderId};
+    return $http.post('api/groups/', groupData);
   }
 }]);
