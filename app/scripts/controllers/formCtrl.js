4c6c9d14eb7094e5a4bf0344bdf5d0efe8ae8a07
Corey Hsu <corey81@gmail.com>
2015-08-17 10:25:21 -0700

    Fixed bug in user view.

--- app/scripts/controllers/formCtrl.js
+++ app/scripts/controllers/formCtrl.js
@@ -1,15 +1,15 @@
-app.controller('FormCtrl', ['$scope', 'FormService', '$location', '$cookies', '$rootScope',
-  function($scope, FormService, $location, $cookies, $rootScope) {
+app.controller('FormCtrl', ['$scope', 'FormService', '$location', '$cookies', '$rootScope', '$window',
+  function($scope, FormService, $location, $cookies, $rootScope, $window) {
   $scope.user = {};
   $scope.signup = function() {
     var user = $scope.user;
-    console.log(user);
     FormService.signup(user).then(function(data) {
       $cookies.put('token', data.token);
       //$cookies.put('user', $scope.user.username);
       $cookies.put('user_id', data.user_id);
       $rootScope.token = data.token;
       $location.path('/');
+      $window.location.reload();
     });
   };
 }]);
