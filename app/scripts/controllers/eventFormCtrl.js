e2145c32224392cf25d18c2aaa6d4c3549c22b38
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-17 11:41:24 -0700

    (feat)form ctrl submits date/time for event

--- app/scripts/controllers/eventFormCtrl.js
+++ app/scripts/controllers/eventFormCtrl.js
@@ -10,12 +10,25 @@ app.controller('EventFormCtrl', ['$scope', '$cookies', 'getGroups', 'CreateEvent
     $scope.myGroups = user.groups;
   });
 
+  // get the current date in correct format
+  var currDateTime = new Date();
+  var currYear = currDateTime.getFullYear();
+  var currMonth = currDateTime.getMonth();
+  var currDay = currDateTime.getDate();
+  var currHour = currDateTime.getHours();
+  var currMinute = currDateTime.getMinutes();
+
+  // get correct datemin format
+  $scope.datemin = currDateTime.toISOString().split('T')[0] + 'T00:00:00';
+  $scope.datetime = new Date(currYear, currMonth, currDay, currHour, currMinute);
+
   $scope.createEvent = function() {
     var newEvent = {
       title: $scope.title,
       group_id: $scope.selectedGroup._id,
       description: $scope.description,
-      user_id: $cookies.get('user_id')
+      user_id: $cookies.get('user_id'),
+      datetime: $scope.datetime
     };
     CreateEvent.newEvent(newEvent).then(function(response) {
       $scope.title = '';
