325be321d20d773f5118701a5289d4fc08e87a82
Andrew Weber <andrew.robert.weber@gmail.com>
2015-08-17 10:12:12 -0700

    adds datetime to event feed and event view, event comments now submitted by logged in user

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -1,20 +1,15 @@
-app.controller('EventController', ['$scope', 'EventService', 'UserService', 'CommentService', '$stateParams',
-  function($scope, EventService, UserService, CommentService, $stateParams) {
+app.controller('EventController', ['$scope', 'EventService', 'UserService', 'CommentService', '$stateParams','$cookies',
+  function($scope, EventService, UserService, CommentService, $stateParams, $cookies) {
+    
     var populateEventData = function(){
       EventService.get($stateParams.id).then(function(response) {
         var data = response.data;
-        $scope.data = data;
-        $scope.eventName = data.title;
-        $scope.group = data.group.groupname;
-        $scope.creator = data.creator.username;
-        $scope.eventDescription = data.description;
-        $scope.votes = data.votes;
-        $scope.upvoters = data.upvoters;
-        $scope.downvoters = data.downvoters;
-        $scope.comments = data.comments;
+        $scope.event = {};
+        $scope.event = data;
+        $scope.user_id = $cookies.get('user_id');
       })
       .then(function(){
-        $scope.comments.forEach(function(comment){
+        $scope.event.comments.forEach(function(comment){
           UserService.get(comment.creator).then(function(response){
             comment.creator = response.data.username;
           });
@@ -23,71 +18,59 @@ app.controller('EventController', ['$scope', 'EventService', 'UserService', 'Com
     };
 
     $scope.postComment = function(){
-      var event_id = $scope.data._id;
+      var event_id = $scope.event._id;
       var comment_text = $scope.comment_text;
 
-      /*
-      ### Get the user_id from the Session object eventually...
-      ### This is just a placeholder
-      */
-      var user_id = "55c5226057c77d1c3bf1453f";
       // Don't submit unless the user has actually written a comment
       if(comment_text){
         var comment = {
           "content": comment_text,
-          "user_id": user_id,
+          "user_id": $scope.user_id,
           "event_id": event_id
         };
+        console.log(comment);
 
         CommentService.post(comment).then(function(data){
+          console.log(data);
           $scope.comment_text = '';
           populateEventData();
         });
       }
     };
 
-    /*
-     ### Like above, get the user_id from the Session object eventually...
-     ### This is just a placeholder
-     */
-    var user_id = "55c5226057c77d1c3bf1453f";
-    $scope.user_id = user_id;
-
     $scope.upvote = function() {
-      if ($scope.upvoters.indexOf(user_id) === -1) {
-        if ($scope.downvoters.indexOf(user_id) >= 0) {
-          $scope.downvoters.splice($scope.downvoters.indexOf(user_id), 1);
-          $scope.data.votes += 2;
+      if ($scope.event.upvoters.indexOf($scope.user_id) === -1) {
+        if ($scope.event.downvoters.indexOf($scope.user_id) >= 0) {
+          $scope.event.downvoters.splice($scope.event.downvoters.indexOf($scope.user_id), 1);
+          $scope.event.votes += 2;
         } else {
-          $scope.data.votes += 1;
+          $scope.event.votes += 1;
         }
-        $scope.data.upvoters.push(user_id);
+        $scope.event.upvoters.push($scope.user_id);
       } else {
-        $scope.upvoters.splice($scope.upvoters.indexOf(user_id), 1);
-        $scope.data.votes -= 1;
+        $scope.event.upvoters.splice($scope.event.upvoters.indexOf($scope.user_id), 1);
+        $scope.event.votes -= 1;
       }
-      var data = $scope.data;
-      EventService.put(data).then(function(data) {
-        $scope.votes = data.data.votes;
+      EventService.put($scope.event).then(function(data) {
+        $scope.event.votes = data.data.votes;
       });
     };
 
     $scope.downvote = function() {
-      if ($scope.downvoters.indexOf(user_id) === -1) {
-        if ($scope.upvoters.indexOf(user_id) >= 0) {
-          $scope.upvoters.splice($scope.upvoters.indexOf(user_id), 1)
-          $scope.data.votes -= 2;
+      if ($scope.event.downvoters.indexOf($scope.user_id) === -1) {
+        if ($scope.event.upvoters.indexOf($scope.user_id) >= 0) {
+          $scope.event.upvoters.splice($scope.event.upvoters.indexOf($scope.user_id), 1)
+          $scope.event.votes -= 2;
         } else {
-          $scope.data.votes -= 1;
+          $scope.event.votes -= 1;
         }
-        $scope.data.downvoters.push(user_id);
+        $scope.event.downvoters.push($scope.user_id);
       } else {
-        $scope.downvoters.splice($scope.downvoters.indexOf(user_id), 1);
-        $scope.data.votes += 1;
+        $scope.event.downvoters.splice($scope.event.downvoters.indexOf($scope.user_id), 1);
+        $scope.event.votes += 1;
       }
-      var data = $scope.data;
-      EventService.put(data).then(function(data) {
-        $scope.votes = data.data.votes;
+      EventService.put($scope.event).then(function(data) {
+        $scope.event.votes = data.data.votes;
       });
     };
 
