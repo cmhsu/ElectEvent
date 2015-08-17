5f4ab54e3599af6afa7a81a9249520077cceb561
Andrew Weber <andrew.robert.weber@gmail.com>
2015-08-17 11:49:23 -0700

    improves individual group view and controller

--- app/scripts/controllers/groupCtrl.js
+++ app/scripts/controllers/groupCtrl.js
@@ -1,5 +1,7 @@
 app.
-  controller('groupCtrl', ['$scope', '$stateParams', '$cookies', '$window', 'getGroups', function ($scope, $stateParams, $cookies, $window, getGroups) {
+  controller('groupCtrl', ['$scope', '$stateParams', '$cookies', '$window', 'getGroups','UserService', 
+    function ($scope, $stateParams, $cookies, $window, getGroups, UserService) {
+    
     var contains = function (array, property, target) {
       return array.reduce(function (prev, curr) {
         return prev ? prev : (curr[property] === target);
@@ -7,15 +9,20 @@ app.
     };
 
     getGroups.getGroup($stateParams.id).then(function (response) {
-      var data = response.data;
-      $scope.groupname = data.groupname;
-      $scope.comments = data.comments;
-      $scope.events = data.events;
-      $scope.members = data.members;
-      $scope.isInGroup = contains(data.members, '_id', $cookies.get('user_id'));
+
+      $scope.data = response.data;
+      $scope.isInGroup = contains($scope.data.members, '_id', $cookies.get('user_id'));
       $scope.joinGroup = function () {
         getGroups.joinGroup($stateParams.id);
         $window.location.reload();
       };
+
+      // Populate the creator field
+      $scope.data.events.forEach(function(event){
+        UserService.get(event.creator).then(function(response){
+          var user = response.data;
+          event.creator = user.username;
+        });
+      });
     });
 }]);
