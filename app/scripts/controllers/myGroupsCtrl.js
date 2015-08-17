3d143a0e6e6634646925fc84c7304913763b5cb1
Corey Hsu <corey81@gmail.com>
2015-08-16 19:39:46 -0700

    My Groups only shows logged-in user's groups.

--- app/scripts/controllers/myGroupsCtrl.js
+++ app/scripts/controllers/myGroupsCtrl.js
@@ -1,6 +1,19 @@
 app
-  .controller('myGroupsCtrl', ['$scope', '$location', 'getGroups', function ($scope, $location, getGroups) {
-    getGroups.getMyGroups().then(function (response) {
-      $scope.groups = response.data;
+  .controller('myGroupsCtrl', ['$scope', '$location', 'getGroups', 'UserService', '$cookies',
+    function ($scope, $location, getGroups, UserService, $cookies) {
+    //getGroups.getMyGroups().then(function (response) {
+    //  $scope.groups = response.data;
+    //  console.log(response.data);
+    //});
+
+    // Manually enter user-id for testing
+    //UserService.get("55cd74f8289cf2d261678944").then(function(response) {
+
+    UserService.get($cookies.get('user_id')).then(function(response) {
+      var user = response.data;
+      console.log(user);
+      $scope.groups = user.groups;
     });
+
+
   }]);
